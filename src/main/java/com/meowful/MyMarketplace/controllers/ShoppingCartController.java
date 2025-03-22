9b7a62054f04aafacd07f41d1819a80ed3037f89
Ivan Zelenov <meowfuljx1@gmail.com>
2025-03-22 15:14:00 +0300

    Current version of project

---
+++ src/main/java/com/meowful/MyMarketplace/controllers/ShoppingCartController.java
@@ -0,0 +1,44 @@
+package com.meowful.MyMarketplace.controllers;
+
+import com.meowful.MyMarketplace.models.ShoppingCart;
+import com.meowful.MyMarketplace.models.User;
+import com.meowful.MyMarketplace.services.ShoppingCartService;
+import com.meowful.MyMarketplace.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class ShoppingCartController {
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+
+    @GetMapping("/show-shopping-cart")
+    public String showShoppingCart(Principal principal, Model model){
+        User user = userService.getUser(principal);
+        ShoppingCart shoppingCart = shoppingCartService.getShoppingCartByUser(user);
+        model.addAttribute("user", user);
+        model.addAttribute("cart", shoppingCart);
+        return "shoppingCart";
+    }
+
+    @PostMapping("/add-to-cart")
+    @ResponseBody
+    public void addProductToCart(Principal principal, @RequestBody Map<String, Object> map){
+        Long productId = Long.parseLong(map.get("productId").toString());
+        shoppingCartService.addProductToCart(principal, productId);
+    }
+
+    @PostMapping("/delete-from-cart")
+    @ResponseBody
+    public Map<String, Integer> deleteProductFromCart(Principal principal, @RequestBody Map<String, Object> map){
+        Long productId = Long.parseLong(map.get("productId").toString());
+        return shoppingCartService.deleteProductFromCart(principal, productId);
+    }
+
+}
---
+++ src/main/java/com/meowful/MyMarketplace/models/Product.java
@@ -0,0 +1,42 @@
+package com.meowful.MyMarketplace.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    @Column(length = 1000)
+    private String description;
+    private int price;
+    private String city;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private User user;
+
+    @ElementCollection(targetClass = String.class, fetch = FetchType.LAZY)
+    @CollectionTable(name = "images", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "image", columnDefinition = "longblob")
+    private List<String> images;
+    @Column(columnDefinition = "longblob")
+    private String previewImg;
+
+    @ManyToOne
+    @JoinColumn(name = "shopping_cart_id")
+    private ShoppingCart shoppingCart;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+}
